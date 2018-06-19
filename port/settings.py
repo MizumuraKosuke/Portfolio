34d6e218fc2b858f392d19468ff1ca9bcbb5ae0b
Mizumura <vocal.loafer@gmail.com>
2018-06-19 17:24:36 +0900

    restart deploy

--- port/settings.py
+++ port/settings.py
@@ -27,7 +27,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PHOTO', 'https')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
